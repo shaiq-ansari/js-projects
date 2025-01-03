1615700d719deb6aa5a83d9cb67b88aa73686909
Shaiq <84909319+shaiq-ansari@users.noreply.github.com>
2025-01-03 19:20:54 +0000

    added prefix and postfix things to clearifying the concepts

--- basics/data_types.js
+++ basics/data_types.js
@@ -5,9 +5,28 @@
 // let score = 12;
 // console.log(typeof (score));
 
-let score = "4a";
-console.log(typeof score)
-console.log(score + 2);
-let scoreInNumber = Number(score);
-console.log(typeof scoreInNumber);
-console.log(scoreInNumber + 2);
+// let score = "4a";
+// console.log(typeof score)
+// console.log(score + 2);
+// let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber + 2);
+
+let a = 23;
+let name = "shaiq";
+let happy = true;
+
+let a2 = a++;
+let name2 = name + "ansari";
+let happyReally = !happy
+
+console.table([a,name,happy]);
+console.table([a2,name2,happyReally]);
+console.log(a2);
+console.log(a2);
+console.log(a2);
+console.log(a2);
+console.log(a2);
+
+let x = a2 + 1;
+console.log(x);
