5381292fc0363ba4d7ec56d7b7d7be1709c3ae56
Shaiq <84909319+shaiq-ansari@users.noreply.github.com>
2025-01-03 14:49:01 +0000

    Number conversion

--- basics/data_types.js
+++ basics/data_types.js
@@ -1,3 +1,13 @@
-const myName = "shaiq";
-const myAge = 23;
-console.table([myName,myAge]);
+// const myName = "shaiq";
+// const myAge = 23;
+// console.table([myName,myAge]);
+
+// let score = 12;
+// console.log(typeof (score));
+
+let score = "4a";
+console.log(typeof score)
+console.log(score + 2);
+let scoreInNumber = Number(score);
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber + 2);
