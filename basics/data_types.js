64f239398b08e5cf3feb38545451298b6bc73304
Shaiq <84909319+shaiq-ansari@users.noreply.github.com>
2025-01-03 20:00:32 +0000

    understood typeof

--- basics/data_types.js
+++ basics/data_types.js
@@ -2,6 +2,10 @@
 // const myAge = 23;
 // console.table([myName,myAge]);
 
+// let a = () => {};
+// console.log(Boolean(a));
+
+console.log(typeof (fn => {}));
 // let score = 12;
 // console.log(typeof (score));
 
@@ -12,21 +16,31 @@
 // console.log(typeof scoreInNumber);
 // console.log(scoreInNumber + 2);
 
-let a = 23;
-let name = "shaiq";
-let happy = true;
+// let a = 23;
+// let name = "shaiq";
+// let happy = true;
 
-let a2 = a++;
-let name2 = name + "ansari";
-let happyReally = !happy
+// let a2 = a++;
+// let name2 = name + "ansari";
+// let happyReally = !happy
 
-console.table([a,name,happy]);
-console.table([a2,name2,happyReally]);
-console.log(a2);
-console.log(a2);
-console.log(a2);
-console.log(a2);
-console.log(a2);
+// console.table([a,name,happy]);
+// console.table([a2,name2,happyReally]);
+// console.log(a2);
+// console.log(a2);
+// console.log(a2);
+// console.log(a2);
+// console.log(a2);
 
-let x = a2 + 1;
-console.log(x);
+// let x = a2 + 1;
+// console.log(x);
+
+// let num = 2;
+// console.log(num);
+// num++;
+// console.log(num);
+
+// let age = 23;
+// let ageAfterOneYear = ++age;
+// console.log(ageAfterOneYear); // 23
+// console.log(age); // 23;
